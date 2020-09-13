b4f1fa1099f8b777e6eb02bdcd003e3db6faa569
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-09-13 20:09:51 +0530

    Token.sol fixed

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.5.11;
 
-import '../node_modules/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 
-
+contract Token is ERC20 {}
 
